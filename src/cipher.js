0a762c9260d84957d915dc91e54a8c5aef851406
luzcarbonell <annacarbonell@gmail.com>
2020-02-20 13:31:37 -0500

    avance

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
 const cipher = {
-  // ...
+  
+}
+cipher.encode = function(offset,string){
+  let str="";
+  for(let i = 0; i<string.length;i++){
+  let ascii = string.charCodeAt(i);
+  if(ascii>=65 && ascii<=90){
+      let num = (ascii - 65 + parseInt(offset)) % 26 + 65;
+      str += String.fromCharCode(num);
+  }
+  else if(ascii>=97 && ascii<=122){
+      let num = (ascii - 97 + parseInt(offset)) % 26 + 97;
+      str += String.fromCharCode(num);
+  }
+  else{
+    str += String.fromCharCode(ascii);
+  }
+}
+  return str;
+};
+cipher.decode = function(offset,string){
+  let str="";
+  for(let i = 0; i<string.length;i++){
+    let ascii = string.charCodeAt(i);
+    if(ascii>=65 && ascii<=90){
+        let num = (ascii - 65 - parseInt(offset)) % 26 + 65;
+        str += String.fromCharCode(num);
+    }
+    else if(ascii>=97 && ascii<=122){
+        let num = (ascii - 122 - parseInt(offset)) % 26 + 122;
+        str += String.fromCharCode(num);
+    }
+    else{
+      str += String.fromCharCode(ascii);
+    }
+  }
+  return str;
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import cipher from './cipher.js';
+console.log("hola");
+let mostrar = document.getElementById("textdes");
+document.getElementById("codifica").addEventListener("click",codi);
+document.getElementById("decodifica").addEventListener("click",deco);
 
-console.log(cipher);
+function codi(){
+    mostrar.innerHTML = cipher.encode(document.getElementById("textint").value,document.getElementById("textcod").value);
+}
+function deco(){
+    mostrar.innerHTML = cipher.decode(document.getElementById("textint").value,document.getElementById("textcod").value);
+
