2b978fc252f062bf130176e84ecc726858e8f92f
luzcarbonell <annacarbonell999@gmail.com>
2020-02-26 16:31:13 -0500

    modificar javascript 26/02/20

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,42 @@
 const cipher = {
-}
-cipher.encode = function(offset,string){
-  let str="";
-  for(let i = 0; i<string.length;i++){
-  let ascii = string.charCodeAt(i);
-  if(ascii>=65 && ascii<=90){
-      let num = (ascii - 65 + parseInt(offset)) % 26 + 65;
-      str += String.fromCharCode(num);
-  }
-  else if(ascii>=97 && ascii<=122){
-      let num = (ascii - 97 + parseInt(offset)) % 26 + 97;
-      str += String.fromCharCode(num);
-  }
-  else{
-    str += String.fromCharCode(ascii);
-  }
-}
-  return str;
-};
-cipher.decode = function(offset,string){
-  let str="";
-  for(let i = 0; i<string.length;i++){
-    let ascii = string.charCodeAt(i);
-    if(ascii>=65 && ascii<=90){
-        let num = (ascii - 90 - parseInt(offset)) % 26 + 90;
-        str += String.fromCharCode(num);
+  encode : (offset,string) => 
+  {
+    let str="";
+    for(let i = 0; i<string.length;i++){
+      let ascii = string.charCodeAt(i);
+      if(ascii>=65 && ascii<=90){
+          let num = (ascii - 65 + parseInt(offset)) % 26 + 65;
+          str += String.fromCharCode(num);
+      }
+      else if(ascii>=97 && ascii<=122){
+          let num = (ascii - 97 + parseInt(offset)) % 26 + 97;
+          str += String.fromCharCode(num);
+      }
+      else{
+        str += String.fromCharCode(ascii);
+      }
     }
-    else if(ascii>=97 && ascii<=122){
-        let num = (ascii - 122 - parseInt(offset)) % 26 + 122;
-        str += String.fromCharCode(num);
+    return str;
+  },
+  decode : (offset,string) =>
+  {
+    let str="";
+    for(let i = 0; i<string.length;i++){
+      let ascii = string.charCodeAt(i);
+      if(ascii>=65 && ascii<=90){
+          let num = (ascii - 90 - parseInt(offset)) % 26 + 90;
+          str += String.fromCharCode(num);
+      }
+      else if(ascii>=97 && ascii<=122){
+          let num = (ascii - 122 - parseInt(offset)) % 26 + 122;
+          str += String.fromCharCode(num);
+      }
+      else{
+        str += String.fromCharCode(ascii);
+      }
     }
-    else{
-      str += String.fromCharCode(ascii);
-    }
+    return str;
   }
-  return str;
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -3,33 +3,47 @@ import cipher from './cipher.js';
 //console.log("hola");
 
 let mostrar = document.getElementById("textdes");
-document.getElementById("codifica").addEventListener("click",codi);
-document.getElementById("decodifica").addEventListener("click",deco);
-document.getElementById("saludo ocultar").classList.add("ocultar");
-document.getElementById("mensaje1").classList.add("ocultar");
-document.getElementById("botones1").classList.add("ocultar");
-document.getElementById("button").addEventListener("click",mos);
+const saludo = document.getElementById("saludo");
+saludo.classList.add("ocultar");
+const mensaje1 = document.getElementById("mensaje1");
+mensaje1.classList.add("ocultar");
+// console.log(mensaje1.class);
+const botones1 = document.getElementById("botones1");
+botones1.classList.add("ocultar");
+const codifica = document.getElementById("codifica");
+const decodifica = document.getElementById("decodifica");
+const intro = document.getElementById("intro");
+const login = document.getElementById("login");
+saludo.classList.remove("ocultar");
 
-function codi(){
-    mostrar.innerHTML = cipher.encode(document.getElementById("textint").value,document.getElementById("textcod").value);
-}
-function deco(){
-    mostrar.innerHTML = cipher.decode(document.getElementById("textint").value,document.getElementById("textcod").value);
-}
 
-function recibirNombre() {
-	
-	const saludo = document.getElementById("saludo");
+const recibirNombre = () => {
 	saludo.innerHTML = "Hola " + document.getElementById('name').value;
-	//console.log(saludo);
 }
-function mos() {
-	
+
+const mos = () => {
+
 	recibirNombre();
-	document.getElementById("mensaje1").classList.remove("ocultar");
-	document.getElementById("botones1").classList.remove("ocultar");
-	document.getElementById("codifica").classList.remove("ocultar");
-    document.getElementById("decodifica").classList.remove("ocultar");
-	document.getElementById("portada").classList.add("ocultar");
-	document.getElementById("saludo ocultar").classList.remove("ocultar");
+	mensaje1.classList.remove("ocultar");
+	botones1.classList.remove("ocultar");
+	codifica.classList.remove("ocultar");
+	decodifica.classList.remove("ocultar");
+	login.classList.add("ocultar");
 }
+
+const codi = () => {
+
+	mostrar.value = cipher.encode(
+		document.getElementById("textint").value,
+		document.getElementById("textcod").value);
+}
+const deco = () => {
+
+	mostrar.value = cipher.decode(
+		document.getElementById("textint").value,
+		document.getElementById("textcod").value);
+}
+
+codifica.addEventListener("click", codi);
+decodifica.addEventListener("click", deco);
+intro.addEventListener("click", mos);
